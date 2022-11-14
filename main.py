9b2bda37a07a697f0c8b58e403ddb360c6b096f8
Ramya66 <68370339+Ramya-Ramkrish@users.noreply.github.com>
2022-11-14 12:40:47 +0000

    This is a commit message.

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import pandas as pd
-df1=pd.read_excel("https://replit.com/@Ramya66/route#BM089_4-11_RAW_DATA.xlsx")
+df1=pd.read_csv("https://replit.com/@Ramya66/route#BM089_4-11_RAW_DATA.csv")
 print(df1)
+
